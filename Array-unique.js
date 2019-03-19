2c82351385c4a6838ce46b69ebb5cfde26ae4e2e
ahabhgk <ahabhgk@gmail.com>
2019-03-19 23:00:57 +0800

    dec

--- Array-unique.js
+++ Array-unique.js
@@ -1,8 +1,4 @@
-// const thisIsAnArray = [1, 1, 2, '1', 3, 4, '4', 3, 4, 2]
-
 const setUniquer = arr => [...new Set(arr)]
-
-// console.log(setUniquer(thisIsAnArray))
 
 const forEachUniquer = (arr) => {
   const res = []
@@ -12,14 +8,10 @@ const forEachUniquer = (arr) => {
   return res
 }
 
-// console.log(forEachUniquer(thisIsAnArray))
-
 const reduceUniquer = arr => arr.reduce((acc, cur) => {
   if (!acc.includes(cur)) acc.push(cur)
   return acc
 }, [])
-
-// console.log(reduceUniquer(thisIsAnArray))
 
 module.exports = {
   setUniquer,
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # three-stupid-array-uniquify-func
 
-Still the same, there are three stupid array unqiuify functions.
+Just like the package name, there are three stupid array unqiuify functions.
+
+## usage
+
+```js
+const { setUniquer ,forEachUniquer, reduceUniquer } = require('three-stupid-array-uniquify-func')
+const thisIsAnArray = [1, 1, 2, '1', 3, 4, '4', 3, 4, 2]
+
+setUniquer(thisIsAnArray)
+forEachUniquer(thisIsAnArray)
+reduceUniquer(thisIsAnArray)
+// All of them output the same array: [1, 2, '1', 3, 4, '4']
+// <doge />
+```
