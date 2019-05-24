e147318f8e0969f7c726b546305f0ebaa724c26e
ahabhgk <ahabhgk@gmail.com>
2019-05-24 23:53:51 +0800

    deepClone

---
+++ deepClone.js
@@ -0,0 +1,26 @@
+function deepClone(target) {
+  // 基本类型拷贝
+  if (!(target instanceof Object)) return target
+  
+  // 引用类型拷贝
+  const result = Array.isArray(target) ? [] : {}
+
+  for (const key in target) {
+    if (target.hasOwnProperty(key)) {
+      if (target[key] === target) { // 判断是否为循环引用
+        result[key] = result // 俺也一样
+      } else {
+        result[key] = deepClone(target[key])
+      }
+    }
+  }
+
+  return result
+}
+
+var a = {
+  arr:[1,2,3,{key:'123'}], // 数组测试
+}
+a.self = a // 循环引用测试
+var c = deepClone(a)
+console.log(c)
